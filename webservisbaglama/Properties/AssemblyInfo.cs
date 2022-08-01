5a6b524c4f4a38ed20798d0b29c1a28fae37cfef
esrefaltug1@gmail.com <esrefaltug1@gmail.com>
2022-08-01 17:44:22 +0300

    Proje dosyası ekle.

---
+++ webservisbaglama/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir derlemeyle ilgili Genel Bilgiler aşağıdaki öznitelik kümesi kullanılarak 
+// denetlenir. Bir derlemeyle ilişkilendirilmiş bilgileri değiştirmek için bu
+// öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("webservisbaglama")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("webservisbaglama")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğini false olarak ayarlarsanız COM bileşenleri bu 
+// derlemedeki türleri göremez. Bu derlemedeki bir türe COM'dan erişmeniz 
+// gerekiyorsa, o türde ComVisible özniteliğini true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'a açılmışsa aşağıdaki GUID typelib'in ID'si içindir
+[assembly: Guid("9a679b14-766d-43b3-babc-21865569046e")]
+
+// Bir derlemenin sürüm bilgisi aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      Alt Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilirsiniz veya Düzeltme ve Yapı Numaralarına aşağıda 
+// gösterildiği gibi '*' ile varsayılan değer atayabilirsiniz:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ webservisbaglama/WebService1.asmx.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Data;
+using Newtonsoft.Json;
+using System.Data.SqlClient;
+
+namespace webservisbaglama
+{
+    /// <summary>
+    /// WebService1 için özet açıklama
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Bu Web Hizmeti'nin, ASP.NET AJAX kullanılarak komut dosyasından çağrılmasına, aşağıdaki satırı açıklamadan kaldırmasına olanak vermek için.
+    // [System.Web.Script.Services.ScriptService]
+    public class WebService1 : System.Web.Services.WebService
+    {
+
+        //[WebMethod]
+        //public string HelloWorld()
+        //{
+        //    return "Merhaba Dünya";
+        //}
+        //public int Sum(int a, int b)
+        //{
+        //    int x = a + b;
+        //    try
+        //    {
+        //        return x;
+        //    }
+        //    catch (FormatException ex1)
+        //    {
+        //        Console.WriteLine("Kullanıcı kaynaklı hata var.Yanlış formmatta giriş yapıldı.");
+        //        return a + b;
+        //    }
+        //}
+
+
+
+        public DataSet SorguCalistir(string query)
+        {
+            SqlConnection con = new SqlConnection("Data Source = localhost\\SQLEXPRESS; Initial Catalog = Mesaa;Integrated Security=True");
+            con.Open();
+            SqlDataAdapter da = new SqlDataAdapter(query, con);
+            da.SelectCommand.ExecuteNonQuery();
+            DataSet dt = new DataSet();
+            da.Fill(dt);
+            return dt;
+        }
+
+        //[WebMethod]
+        //public string POST(int Id,string Name,String SurName,string Email)
+        //{
+        //    string str1 = Id.ToString();
+        //    string connString = ("Server=localhost\\SQLEXPRESS; Database= Mesaa;Trusted_Connection=True;MultipleActiveResults=true");
+        //    string query = "INSERT INTO demoJSON(Id,Name,SurName,Email)VALUES('" + str1 + "','" + Name + "','" + SurName + "','" + Email + "')";//post metot
+        //    try
+        //    {
+        //        using (SqlConnection conn=new SqlConnection(connString))
+        //        {
+        //            using (SqlCommand command=new SqlCommand(query,conn))
+        //            {
+        //                conn.Open();
+        //                command.ExecuteNonQuery();
+        //                return "Harika";
+        //            }
+        //            conn.Close();
+        //        }
+        //    }
+        //    catch (SystemException ex)
+        //    {
+
+        //        return "Sistem Hatası";
+        //    }
+
+        //}
+
+        [WebMethod]
+        public DataSet GET()
+        {
+            string query = "Select * from BILGISAYARLAR4"; //Get metot
+            return SorguCalistir(query);
+        }
+        [WebMethod]
+        public DataSet DELETE(string dpo)
+        {
+            string query = "DELETE FROM DisplayCikislari";   //Delete metot
+            return SorguCalistir(query);
+        }
+        [WebMethod]
+        public DataSet POST( string Name, String SurName, string Email)
+        {
+            
+            string query = "INSERT INTO demoJSON(Name,SurName,Email)VALUES('" + Name + "','" + SurName + "','" + Email + "')";//post metot
+            
+            return SorguCalistir(query);
+        }
+        [WebMethod]
+       public DataSet PUT(int id,int hdm)
+        {
+            string query = "UPDATE DisplayCikislari SET HDMI="+hdm+"WHERE Id="+id;
+            return SorguCalistir(query);
+        }
+
+       
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
